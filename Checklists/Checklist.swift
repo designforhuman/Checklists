b7139bd9a0840208c619cdb336ca099fef48a740
David Lee <designforhuman@gmail.com>
2016-09-20 18:23:04 +0900

    07 - Lists

---
+++ Checklists/Checklist.swift
@@ -0,0 +1,13 @@
+//
+//  Checklist.swift
+//  Checklists
+//
+//  Created by LeeDavid on 9/19/16.
+//  Copyright © 2016 Daylight. All rights reserved.
+//
+
+import UIKit
+
+class Checklist: NSObject {
+
+}
---
+++ Checklists/ListDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ListDetailViewController.swift
+//  Checklists
+//
+//  Created by LeeDavid on 9/19/16.
+//  Copyright © 2016 Daylight. All rights reserved.
+//
+
+import Foundation
